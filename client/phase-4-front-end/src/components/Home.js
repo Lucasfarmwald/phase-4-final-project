c23fc8b215e02997cc05fbba39e65de180db7118
lucasfarmwald <farmwaldlucas@gmail.com>
2022-09-07 13:32:05 -0500

    added client

--- client/phase-4-front-end/src/components/Home.js
+++ client/phase-4-front-end/src/components/Home.js
@@ -10,11 +10,21 @@ function Home() {
 
   return (
     <div className="house">
+      <img
+        className="pictureofhouse"
+        alt=""
+        src="https://a0.muscache.com/im/pictures/09efb5fc-7bf1-44c0-b0ef-241a38533e67.jpg?im_w=1680"
+      />
       <form onSubmit={handleChange}>
+        <h1>Visiting Tulsa?</h1>
         <button>
           <span id="span">Let's find a Haus...</span>
         </button>
       </form>
+      <img
+        alt=""
+        src="https://a0.muscache.com/im/pictures/5b4dc94a-0b4c-4c27-b50f-9c5a5b93c775.jpg?im_w=720"
+      />
     </div>
   );
 }
