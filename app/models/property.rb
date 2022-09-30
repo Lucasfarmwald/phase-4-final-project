10f6d8cd8ed2bac08f7d519a66cc2eb510fcace4
lucasfarmwald <farmwaldlucas@gmail.com>
2022-09-30 16:27:11 -0500

    added new associations for user and property class

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,4 @@
 class Property < ApplicationRecord
     has_many :reviews
-    has_many :users
+    has_many :users, through: :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_secure_password
 
     has_many :reviews
-    has_many :properties
+    has_many :properties, through: :reviews
 
     validates :username, presence: true
 
